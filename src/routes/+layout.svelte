b9b204f0d21ebb9111625f03a52dfbe1c4fc0d08
Daniel George <programmerpremium@gmail.com>
2023-01-27 19:05:27 +0000

    style: add roboto mono font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import "../app.css";
 </script>
 
-<div class="dark bg-zinc-100 dark:bg-zinc-800 min-h-screen">
+<div class="dark bg-zinc-100 dark:bg-zinc-800 min-h-screen font-rb-mono">
 	<slot />
 </div>
