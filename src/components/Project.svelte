0543b84fce8141505cab0a3f2e0020ef66a39f81
Daniel George <programmerpremium@gmail.com>
2023-01-27 19:40:24 +0000

    feat: add changeable color on project component

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -3,9 +3,10 @@
 	export let date: string;
 	export let description: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente molestiae unde quae quis quasi? Error!";
 	export let image: string = "https://via.placeholder.com/80"
+	export let color: string = "bg-white";
 </script>
 
-<article class="flex gap-4 bg-opacity-30 p-4 my-3 rounded-md hover:scale-105 transform ease-in-out duration-300 bg-purple-500">
+<article class={`flex gap-4 bg-opacity-30 p-4 my-3 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color}`}>
 	<img class="rounded-md" src={image} alt="Project">
 	<div>
 		<h3 class="font-bold text-lg flex items-center">{title} <span class="font-normal text-sm ml-4">[{date}]</span></h3>
