dec1a8342c1d07f355fcfcab6c19b37f81eb814e
Daniel George <programmerpremium@gmail.com>
2023-01-27 22:55:08 +0000

    feat: replaced lorem ipsum

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	export let color: string = "bg-white";
 	export let title: string;
-	export let url: string = "";
+	export let url: string | null;
 </script>
 
-<a href={url} target="_blank" class={`flex gap-4 bg-opacity-30 p-4 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color} cursor-pointer`}>
+<a href={url} rel="noreferrer" target="_blank" class={`flex gap-4 bg-opacity-30 p-4 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color} cursor-pointer`}>
 	<slot class="w-6" />
 	<h3>{title}</h3>
 </a>
