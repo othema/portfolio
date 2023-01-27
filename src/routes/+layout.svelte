68d52fb15fc1592dd64214723e1d2349ba99fa1e
Daniel George <programmerpremium@gmail.com>
2023-01-27 19:06:24 +0000

    fix: made text white on dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import "../app.css";
 </script>
 
-<div class="dark bg-zinc-100 dark:bg-zinc-800 min-h-screen font-rb-mono">
+<div class="dark bg-zinc-100 dark:bg-zinc-800 min-h-screen font-rb-mono text-zinc-900 dark:text-white">
 	<slot />
 </div>
