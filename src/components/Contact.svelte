6e3755877e9b7b6a0fa5101b94d75a348f86e65d
Daniel George <programmerpremium@gmail.com>
2023-01-27 22:10:11 +0000

    feat: add avatar

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let color: string = "bg-white";
 	export let title: string;
+	export let url: string = "";
 </script>
 
-<div class={`flex gap-4 bg-opacity-30 p-4 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color}`}>
+<a href={url} target="_blank" class={`flex gap-4 bg-opacity-30 p-4 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color} cursor-pointer`}>
 	<slot class="w-6" />
 	<h3>{title}</h3>
-</div>
+</a>
--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -2,12 +2,12 @@
 	export let title: string;
 	export let date: string;
 	export let description: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente molestiae unde quae quis quasi? Error!";
-	export let image: string = "https://via.placeholder.com/70"
+	export let image: string = "https://via.placeholder.com/48"
 	export let color: string = "white";
 </script>
 
-<article class={`flex gap-4 bg-opacity-30 p-4 my-3 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color}`}>
-	<img class="rounded-md" src={image} alt="Project">
+<article class={`flex items-center gap-4 bg-opacity-30 p-4 my-3 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color}`}>
+	<img class="rounded-md h-12 w-12" src={image} alt="Project">
 	<div>
 		<h3 class="font-bold text-lg flex items-center">{title} <span class="font-normal text-sm ml-4">[{date}]</span></h3>
 		<p class="text-sm">{description}</p>
