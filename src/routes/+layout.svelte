e01505a7268ce46ec1f7ec7b271d571b3e9d98dc
Daniel George <programmerpremium@gmail.com>
2023-01-27 19:10:10 +0000

    feat: add column layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import "../app.css";
 </script>
 
-<div class="dark bg-zinc-100 dark:bg-zinc-800 min-h-screen font-rb-mono text-zinc-900 dark:text-white">
+<div class="dark bg-zinc-100 dark:bg-[#191919] min-h-screen font-rb-mono text-[#191919] dark:text-[#ebf4f1]">
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+	djvgeorge.me
+</nav>
+
+<div class="max-w-3xl px-8 mt-24 mx-auto">
+	<h1 class="font-bold text-3xl">Daniel George</h1>
+</div>
