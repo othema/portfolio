b527ca9fc95a929fcd5dd9cf7dd576e67e32b418
Daniel George <programmerpremium@gmail.com>
2023-01-27 19:30:33 +0000

    feat: add about section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,22 @@
 </nav>
 
 <div class="max-w-3xl px-8 mt-24 mx-auto">
-	<h1 class="font-bold text-3xl">Daniel George</h1>
+	<section>
+		<h1 class="font-bold text-3xl">Daniel George</h1>
+		<p class="w-[80%] mt-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus, ipsa architecto incidunt nesciunt obcaecati at inventore fugiat eum.</p>
+		<img class="h-11 mt-4" src="https://skillicons.dev/icons?perline=30&i=js,ts,html,css,nodejs,react,tailwind,svelte,sass,py,c,unity&theme=dark" alt="My skills" />
+	</section>
+
+	<hr class="my-8 border-gray-400 dark:border-gray-500"/>
+
+	<section>
+		<h2 class="font-bold text-2xl">Notable Projects</h2>
+
+		<ul class="mt-3">
+			<article class="bg-opacity-30 p-4 my-2 rounded-md hover:scale-105 transform ease-in-out duration-300 bg-purple-500">
+				<h3 class="font-bold text-lg flex items-center">StockX <span class="font-normal text-sm ml-4">[Mar 2021]</span></h3>
+				<p class="text-sm mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente molestiae unde quae quis quasi? Error!</p>
+			</article>
+		</ul>
+	</section>
 </div>
