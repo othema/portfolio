5beb7ad24d07965ee160ced21b50ea9b08c4f73c
Daniel George <programmerpremium@gmail.com>
2023-01-28 09:34:40 +0000

    perf: accessibility link aria-labels

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -4,7 +4,9 @@
 	export let url: string | null;
 </script>
 
-<a href={url} rel="noreferrer" target="_blank" class={`flex gap-4 bg-opacity-30 p-4 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color} cursor-pointer`}>
-	<slot class="w-6" />
-	<h3>{title}</h3>
-</a>
+<li>
+	<a href={url} aria-label={"Contact " + title} rel="noreferrer" target="_blank" class={`flex gap-4 bg-opacity-30 p-4 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color} cursor-pointer`}>
+		<slot class="w-6" />
+		<h3>{title}</h3>
+	</a>
+</li>
