ad36301e822c76f71b96c7c6c39d9ddc73099597
Daniel George <programmerpremium@gmail.com>
2023-01-28 09:54:53 +0000

    fix: fixed dark theme bright colors

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -3,12 +3,13 @@
 	export let date: string;
 	export let description: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente molestiae unde quae quis quasi? Error!";
 	export let image: string = "https://via.placeholder.com/48"
-	export let color: string = "white";
+	export let color: string = "gray-500";
+	export let darkColor: string = "white";
 </script>
 
 <li>
 	<a href="#" aria-label={"Visit" + title}>
-		<article class={`flex items-center gap-4 bg-opacity-30 p-4 my-3 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color}`}>
+		<article class={`flex items-center gap-4 p-4 my-3 rounded-md hover:scale-105 transform ease-in-out duration-300 ${color} dark:${darkColor} bg-opacity-30`}>
 			<img width={48} height={48} class="rounded-md h-12 w-12" src={image} alt="Project">
 			<div>
 				<h3 class="font-bold text-lg flex items-center">{title} <span class="font-normal text-sm ml-4">[{date}]</span></h3>
